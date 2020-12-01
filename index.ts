bfe78222367d8e8f8d360b6d665e6635f9910eb5
Vivek Lakshmanan <vivek@pulumi.com>
2020-11-30 18:35:00 -0800

    Quick change

--- index.ts
+++ index.ts
@@ -2,6 +2,5 @@ const Font = require('ascii-art-font');
 
 Font.fontPath = './node_modules/ascii-art-font/Fonts/'
 Font.create('it works!', 'Doom', function (err: any, rendered: any) {
-    console.log('here?')
     console.log(rendered)
 });
